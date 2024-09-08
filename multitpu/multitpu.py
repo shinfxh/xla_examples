6a0988bfd135924fe770cf0bc30c85a63a048162
shinfxh <fuxinghong1@gmail.com>
2024-09-08 06:42:16 +0000

    included instructions for profiling

--- multitpu/multitpu.py
+++ multitpu/multitpu.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from datautils import MyTrainDataset
@@ -11,6 +12,7 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
+import torch_xla.debug.profiler as xp
 import os
 
 
@@ -37,7 +39,7 @@ class Trainer:
     ) -> None:
         self.gpu_id = gpu_id
         # self.model = model.to(gpu_id)
-        self.model = model.to(xm.xla_device())
+        self.model = model.to(xla.device())
         self.train_data = train_data
         self.optimizer = optimizer
         self.save_every = save_every
@@ -51,6 +53,8 @@ class Trainer:
         self.optimizer.step()
 
     def _run_epoch(self, epoch):
+        # profile_logdir = os.getcwd()
+        # xp.trace_detached('localhost:9012', profile_logdir)
         b_sz = len(next(iter(self.train_data))[0])
         print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
         self.train_data.sampler.set_epoch(epoch)
@@ -74,8 +78,30 @@ class Trainer:
 
 
 def load_train_objs():
-    train_set = MyTrainDataset(2048)  # load your dataset
-    model = torch.nn.Linear(20, 1)  # load your model
+    train_set = MyTrainDataset(16384)  # load your dataset
+    # model = torch.nn.Linear(20, 1)  # load your model
+    w = 2000
+    model = nn.Sequential(
+            nn.Linear(20, w),  # First layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Second layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Third layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Fourth layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Fifth layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Sixth layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Seventh layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Eighth layer
+            nn.ReLU(),
+            nn.Linear(w, w),  # Ninth layer
+            nn.ReLU(),
+            nn.Linear(w, 1)  # Final output layer
+        )
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
     return train_set, model, optimizer
 
@@ -91,6 +117,7 @@ def prepare_dataloader(dataset: Dataset, batch_size: int):
 
 
 def main(rank: int, world_size: int, save_every: int, total_epochs: int, batch_size: int):
+    server = xp.start_server(9012)
     ddp_setup(rank, world_size)
     dataset, model, optimizer = load_train_objs()
     train_data = prepare_dataloader(dataset, batch_size)
@@ -104,7 +131,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
-    parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
+    parser.add_argument('--batch_size', default=1024, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
     
     # world_size = torch.cuda.device_count()
